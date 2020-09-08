1c9bf68d89070ce09115cf8f1561ef2509edc9f3
LinaProfikit <angelina@profitkit.ru>
2020-09-08 19:46:33 +0300

    make litle chges.

---
+++ signal/templates/components/partners/template.php
@@ -0,0 +1,57 @@
+<section class="main__partners">
+    <div id="particles-js__partners" class="particles main__partners_particles">
+
+    </div>
+
+    <div class="container">
+        <div class="main__partners_head head">
+            <h2>
+                Наши заказчики
+            </h2>
+            <div class="head__line"></div>
+        </div>
+
+
+
+        <div class="main__partners_slider">
+            <div class="partnersSlider">
+                <div class="slick-item main__partners_slick-item">
+                    <div class="slick_item_block">
+                        <img class="" src="templates/assets/img/paa.png"/>
+                    </div>
+
+
+                </div>
+                <div class="slick-item main__partners_slick-item">
+
+                  <div class="slick_item_block">
+                      <img src="templates/assets/img/owox.png"/>
+                  </div>
+                </div>
+                <div class="slick-item main__partners_slick-item">
+                    <div class="slick_item_block">
+                        <img src="templates/assets/img/bobruisk.png"/>
+                    </div>
+                </div>
+                <div class="slick-item main__partners_slick-item">
+                    <div class="slick_item_block">
+                        <img src="templates/assets/img/panter.png"/>
+                    </div>
+
+                </div>
+                <div class="slick-item main__partners_slick-item">
+                    <div class="slick_item_block">
+                        <img src="templates/assets/img/ucraft.png"/>
+                    </div>
+                </div>  <div class="slick-item main__partners_slick-item">
+                    <div class="slick_item_block">
+                        <img src="templates/assets/img/ucraft.png"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+
